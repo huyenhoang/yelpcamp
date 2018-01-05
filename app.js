a5171555f4b8b9fb2a90428ea3a3544b01b610ab
Huyen Hoang <huyenhoang33@gmail.com>
2018-01-05 18:24:32 +0000

    refactor: new comment routes

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express      = require("express"),
     bodyParser   = require("body-parser"),
     mongoose     = require("mongoose"),
     Campground   = require("./models/campground"),
+    Comment      = require("./models/comment"),
     seedDB       = require("./seeds")
     
-// connect to db
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -86,6 +86,30 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
             res.render("comments/new", {campground: campground});
         }
     })
+})
+
+app.post("/campgrounds/:id/comments", function(req, res){
+    //lookup campground using ID
+    Campground.findById(req.params.id, function(err, campground){
+       if(err){
+           console.log(err);
+           res.redirect("/campgrounds");
+       } else {
+           Comment.create(req.body.comment, function(err, comment){
+               if(err){
+                   console.log(err);
+               } else {
+                   campground.comments.push(comment);
+                   campground.save();
+                   res.redirect('/campgrounds/' + campground._id);
+               }
+           });
+       }
+    });
+    //create new comment
+    //connect new comment to campground
+    //redirect to campground show page
+    
 })
 
 app.listen(process.env.PORT, process.env.IP, function(){
