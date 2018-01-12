9d041cd9bbae002dee671798de548ff0feeb1758
Huyen Hoang <huyenhoang33@gmail.com>
2018-01-12 04:40:17 +0000

    feat: added auth for login & register pages

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ app.get("/campgrounds/:id", function(req, res){
 // COMMENTS ROUTES
 // ====================
 
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
     // find campground by id
     Campground.findById(req.params.id, function(err, campground){
         if(err){
@@ -97,7 +97,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
     })
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
    //lookup campground using ID
    Campground.findById(req.params.id, function(err, campground){
        if(err){
@@ -150,10 +150,24 @@ app.get("/login", function(req, res){
 app.post("/login", passport.authenticate("local", 
     {
         successRedirect: "/campgrounds", 
-        failureRedirect: "/login"
+        failureRedirect: "/login",
     }), function(req, res) {    
         
 });
+
+// logout route
+app.get("/logout", function(req, res){
+   req.logout();
+   res.redirect("/campgrounds");
+   
+});
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+      return next();
+  }
+    res.redirect("/login");
+}
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started!");
