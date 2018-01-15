43fb5f58b54cc32df6990a3e81e3016db5b4a478
Huyen Hoang <huyenhoang33@gmail.com>
2018-01-15 18:18:28 +0000

    refactor: middleware added for campgrounds, comments, and auth

--- middleware/index.js
+++ middleware/index.js
@@ -1,3 +1,53 @@
-var Campground = require("../models/campgrounds");
-var Comment = require("../models/comments");
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
 
+// middle ware codes
+
+var middlewareObj = {};
+
+middlewareObj.isLoggedIn = function(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+        res.redirect("/login");
+}
+
+middlewareObj.checkCommentOwnership = function(req, res, next) {
+    if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment) {                
+            if(err){
+                res.redirect("back")
+            } else {
+                // does user own comment?
+                if(foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
+}
+
+middlewareObj.checkCampgroundOwnership = function(req, res, next) {
+    if(req.isAuthenticated()){
+        Campground.findById(req.params.id, function(err, foundCampground) {                
+            if(err){
+                res.redirect("back")
+            } else {
+                // does user own campground?
+                if(foundCampground.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
+}
+
+module.exports = middlewareObj;
